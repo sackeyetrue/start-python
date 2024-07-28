d29a11be86594782073aac40b93d42f99a9f1fb7
zeke <sackeyetrue@gmail>
2024-07-28 20:50:47 +0000

    new app  added (live)

--- beginner/app.py
+++ beginner/app.py
@@ -1,3 +1,9 @@
 # Live on Earth
-# Calculate Months,Days and Minutes spent on Earth
+
+print("Calculate Months,Days and Minutes spent on Earth: ")
+
+userAge = int(input("Enter Your Age: "))
+
+print(userAge)
+
 
